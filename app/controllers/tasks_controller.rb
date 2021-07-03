8980724f673a4ac38a9a4a2bbb02c8747b170c5f
hikaru <thieryhikarin0851@gmail.com>
2021-07-03 12:52:49 +0000

    update after mentor's review1

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,10 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:update, :destroy]
   
   def index
-    if logged_in?
-      @task = current_user.tasks.build  # form_with 用
-      @pagy, @tasks = pagy(current_user.tasks.order(id: :desc))
-    end
+    @task = current_user.tasks.build  # form_with 用
+    @pagy, @tasks = pagy(current_user.tasks.order(id: :desc))
   end
   
   def show
@@ -58,12 +55,5 @@ class TasksController < ApplicationController
   
   def set_task
     @task = Task.find(params[:id])
-  end
-  
-  def correct_user
-    @task = current_user.tasks.find_by(id: params[:id])
-    unless @task
-      redirect_to root_url
-    end
   end
 end
